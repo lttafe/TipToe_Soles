88348b9fffaacec514f867cce7265119c9c076b9
lttafe <lachlan.tanfield2@studytafensw.edu.au>
2024-11-12 11:11:04 +0000

    Added redirect for when on null.html page

---
+++ js/null_redirect.js
@@ -0,0 +1,8 @@
+// LACHLAN TANFIELD - PART 3 ASSIGNMENT
+
+// REDIRECTS TO index.html after 5 seconds
+window.onload = function() {
+    setTimeout(function() {
+        window.location.href = "index.html";
+    }, 5000);
+};
